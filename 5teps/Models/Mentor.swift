5ad72fb5fdf7b286e0522ffa68a8d12e3b87e432
Giusy Di Paola <giusydipaola@icloud.com>
2020-05-08 14:41:41 +0200

    Classi Onboarding + Mentor

---
+++ 5teps/Models/Mentor.swift
@@ -0,0 +1,30 @@
+//
+//  Mentor.swift
+//  5teps
+//
+//  Created by Giusy Di Paola on 08/05/2020.
+//  Copyright © 2020 Mario Armini. All rights reserved.
+//
+
+import UIKit
+
+class Mentor: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
