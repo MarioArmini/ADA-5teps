9366ab8f354fc2eef084831cd90e06d35e105102
Giusy Di Paola <giusydipaola@icloud.com>
2020-05-08 23:06:42 +0200

    xib Mentor Subview

---
+++ 5teps/Models/Subview.swift
@@ -0,0 +1,17 @@
+//
+//  Subview.swift
+//  5teps
+//
+//  Created by Giusy Di Paola on 08/05/2020.
+//  Copyright © 2020 Mario Armini. All rights reserved.
+//
+
+import UIKit
+
+class Subview: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textView: UILabel!
+
+}
