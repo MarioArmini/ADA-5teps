7619f0bf30f8c068cbc89f702165efe0c6351971
MarioArmini <arminimario10@gmail.com>
2020-05-09 14:47:26 +0200

    CollectionView per i Topic

--- 5teps/Controllers/FirstViewController.swift
+++ 5teps/Controllers/FirstViewController.swift
@@ -11,14 +11,14 @@ import UIKit
 class FirstViewController: UIViewController {
     
     @IBOutlet weak var viewMentorTop: UIView!
-    @IBOutlet weak var topicLabelTest: UILabel!
+    @IBOutlet weak var TopicCollectionView: UICollectionView!
     var referenceForViewTop : Subview?
+    let topics = Topic.list()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // MARK: Mentor top view settings ------------------------------------
         if let referenceForViewOnTheTop = Bundle.main.loadNibNamed("Subview", owner: self, options: nil)?.first as? Subview  {
             viewMentorTop.addSubview(referenceForViewOnTheTop)
@@ -42,14 +42,15 @@ class FirstViewController: UIViewController {
         }
     }
     override func viewWillAppear(_ animated: Bool) {
-        topicLabelTest.text = ""
+        TopicCollectionView.reloadData()
+       /* topicLabelTest.text = ""
         let topics = Topic.list()
         for topic in topics {
             print(topic)
             topicLabelTest.text = topicLabelTest.text! + "\n \(String(describing: topic.name)) - \(String(describing: topic.icon))"
         }
         // MARK: Esempio di navigazione oggetti Topic -> Challenge -> Step
-        /*
+        
          for t in topics {
              print("Topic: \(t.name!)")
              let challenges = t.findChallengeByState(state: ChallengeState.Create)
@@ -62,6 +63,48 @@ class FirstViewController: UIViewController {
              print("###########################################")
          }
          */
+    }
+}
+
+extension FirstViewController: UICollectionViewDelegate{
+    
+}
+
+extension FirstViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+   
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return topics.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! TopicCollectionViewCell
+        cell.TopicLabel.text = self.topics[indexPath.section].name
+        //cell.TopicIconView.image = topics[indexPath.section].icon?.emojiToImage()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+               return CGSize(width: 373, height: 528)
+       }
+    
+}
+
+extension String {
+    func emojiToImage() -> UIImage? {
+        let size = CGSize(width: 30, height: 35)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        UIColor.white.set()
+        let rect = CGRect(origin: CGPoint(), size: size)
+        UIRectFill(rect)
+        (self as NSString).draw(in: rect, withAttributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 30)])
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
     }
 }
 
---
+++ 5teps/Models/TopicCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TopicCollectionViewCell.swift
+//  5teps
+//
+//  Created by Mario Armini on 09/05/2020.
+//  Copyright © 2020 Mario Armini. All rights reserved.
+//
+
+import UIKit
+
+class TopicCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var TopicLabel: UILabel!
+    @IBOutlet weak var TopicIconView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
