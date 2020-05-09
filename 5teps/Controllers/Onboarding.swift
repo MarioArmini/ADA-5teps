f80d8cc5f90fe1ccc6eac12d6cfdd6a50e9fbed5
Giusy Di Paola <giusydipaola@icloud.com>
2020-05-09 22:58:13 +0200

    [Onboarding] View (xib), Slide, Page Control Setup

--- 5teps/Controllers/Onboarding.swift
+++ 5teps/Controllers/Onboarding.swift
@@ -8,23 +8,77 @@
 
 import UIKit
 
-class Onboarding: UIViewController {
-
+class Onboarding: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var skipButton: UIButton!
+    
+    var slides:[Slide] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        scrollView.delegate = self
+        
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        scrollView.isExclusiveTouch = true
+        self.scrollView.canCancelContentTouches = true
+        self.scrollView.delaysContentTouches = true
+        view.bringSubviewToFront(pageControl)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createSlides() -> [Slide] {
+        
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.imageView.image = UIImage(named: "mentor")
+        
+        
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide2.imageView.image = UIImage(named: "mentor")
+        
+        
+        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide3.imageView.image = UIImage(named: "mentor")
+        
+        return [slide1, slide2, slide3]
     }
-    */
-
+    
+    func setupSlideScrollView(slides : [Slide]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+        
+        if pageIndex == 2{
+            changeTitle()
+        }else{
+            skipButton.setTitle("Skip", for: .normal)
+            
+        } 
+        if scrollView.contentOffset.y > 0 || scrollView.contentOffset.y < 0 {
+            scrollView.contentOffset.y = 0
+        }
+    }
+    func changeTitle() {
+        skipButton.setTitle("Start", for: .normal)
+        
+    }
+    
 }
---
+++ 5teps/View/OnBoarding/Slide.swift
@@ -0,0 +1,22 @@
+//
+//  Slide.swift
+//  5teps
+//
+//  Created by Giusy Di Paola on 09/05/2020.
+//  Copyright © 2020 Mario Armini. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
