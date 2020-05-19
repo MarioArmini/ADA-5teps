64124945a903716a3101fddb6ebd939c74e34889
Fabio Palladino <fa.palladino@gmail.com>
2020-05-19 17:30:19 +0200

    prima versione card challenge con gli step

--- 5teps/View/CardCollectionViewCell.swift
+++ 5teps/View/CardCollectionViewCell.swift
@@ -12,12 +12,21 @@ class CardCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var viewCard: UIView!
+    @IBOutlet weak var step1: UIImageView!
+    @IBOutlet weak var step2: UIImageView!
+    @IBOutlet weak var step3: UIImageView!
+    @IBOutlet weak var step4: UIImageView!
+    @IBOutlet weak var step5: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    var gradientLayer: CAGradientLayer?
+
     
     var back: UIView!
     var front: UIView!
     
     var initNumber:Int = 0
     var flipped = true
+    let cornerRadius: CGFloat = 20.29
     var challenge: Challenge? {
         didSet {
             updateUI()
@@ -33,8 +42,8 @@ class CardCollectionViewCell: UICollectionViewCell {
         
         back.isHidden = !flipped
         
-        front.layer.borderWidth = 2.0
-        back.layer.borderWidth = 2.0
+        //front.layer.borderWidth = 2.0
+        //back.layer.borderWidth = 2.0
         
         let editButton = UIButton()
         let deleteButton = UIButton()
@@ -48,13 +57,13 @@ class CardCollectionViewCell: UICollectionViewCell {
         back.backgroundColor = UIColor.clear
         front.backgroundColor = UIColor.clear
         
-        front.layer.cornerRadius = 20
+        front.layer.cornerRadius = cornerRadius
         front.clipsToBounds = true
-        front.layer.borderWidth = 2.0
+        //front.layer.borderWidth = 2.0
         
-        back.layer.cornerRadius = 20
+        back.layer.cornerRadius = cornerRadius
         back.clipsToBounds = true
-        back.layer.borderWidth = 2.0
+        //back.layer.borderWidth = 2.0
         
         back.addSubview(deleteButton)
         back.addSubview(editButton)
@@ -107,13 +116,63 @@ class CardCollectionViewCell: UICollectionViewCell {
     }
     
     func updateUI() {
-        viewCard.layer.cornerRadius = 20
+        var imageStep = [Int64:UIImageView]()
+        imageStep[1] = step1
+        imageStep[2] = step2
+        imageStep[3] = step3
+        imageStep[4] = step4
+        imageStep[5] = step5
+        
+        
+        viewCard.layer.cornerRadius = cornerRadius
         viewCard.clipsToBounds = true
-        viewCard.backgroundColor = challenge?.topic?.bgColor
-        viewCard.layer.borderWidth = 2.0
-        viewCard.layer.borderColor = UIColor.black.cgColor
+        //viewCard.backgroundColor = challenge?.topic?.bgColor
+        //viewCard.layer.borderWidth = 2.0
+        //viewCard.layer.borderColor = UIColor.black.cgColor
         
         nameLabel.text = challenge?.name
+        
+        if let topic = challenge?.topic {
+            //Card Layout
+            contentView.layer.cornerRadius = cornerRadius
+            contentView.clipsToBounds = true
+            let color = topic.colorCard
+            self.imageView.image = UIImage.loadTopicIcon(name: topic.icon ?? "")
+            
+            //print(topic?.name, color,topic?.color)
+            gradientLayer?.removeFromSuperlayer()
+            gradientLayer = CAGradientLayer()
+            gradientLayer?.colors = [color.uiColor1.cgColor, color.uiColor2.cgColor]
+            gradientLayer?.startPoint = CGPoint(x: 0.0, y: 0.0)
+            gradientLayer?.endPoint = CGPoint(x: 1.0, y: 1.0)
+            gradientLayer?.locations = [0, 1]
+            gradientLayer?.frame = viewCard.bounds
+            
+            viewCard.layer.insertSublayer(gradientLayer!, at: 0)
+        }
+        if challenge != nil {
+            let steps = challenge!.stepsOrder
+            var maxStep: Int64 = 1
+            for s in steps {
+                var icon = "step-empty"
+                if s.isFinish {
+                    icon = "step-fill"
+                }
+                if imageStep.keys.contains(s.step) {
+                    imageStep[s.step]?.image = UIImage(named: icon)
+                    imageStep[s.step]?.isHidden = false
+                }
+                maxStep = (maxStep < s.step) ? s.step : maxStep
+            }
+            //Se sono meno di 5 step nascondo le immagini
+            maxStep = maxStep + 1
+            if maxStep < 5 {
+                for i in maxStep...5 {
+                    imageStep[i]?.isHidden = true
+                }
+            }
+        }
+
     }
     
 }
